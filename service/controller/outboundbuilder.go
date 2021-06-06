5680f443d028b6a04fa6efcb0f0d69331d33bd5d
amfiyong <vionsenthis@gmail.com>
2021-06-06 21:50:02 +0800

    support customalize send ip

--- service/controller/outboundbuilder.go
+++ service/controller/outboundbuilder.go
@@ -5,22 +5,34 @@ import (
 	"fmt"
 
 	"github.com/XrayR-project/XrayR/api"
+	"github.com/xtls/xray-core/common/net"
 	"github.com/xtls/xray-core/core"
 	"github.com/xtls/xray-core/infra/conf"
 )
 
 //OutboundBuilder build freedom outbund config for addoutbound
-func OutboundBuilder(nodeInfo *api.NodeInfo, EnableDNS bool) (*core.OutboundHandlerConfig, error) {
+func OutboundBuilder(config *Config, nodeInfo *api.NodeInfo) (*core.OutboundHandlerConfig, error) {
 	outboundDetourConfig := &conf.OutboundDetourConfig{}
 	outboundDetourConfig.Protocol = "freedom"
 	outboundDetourConfig.Tag = fmt.Sprintf("%s_%d", nodeInfo.NodeType, nodeInfo.Port)
-	// Protocol setting
-	var dnsSettings string = "Asis"
-	if EnableDNS {
-		dnsSettings = "UseIP"
+
+	// build send ip address
+	if config.SendIP != "" {
+		ipAddress := net.ParseAddress(config.ListenIP)
+		outBoundConfig.SendThrough = &conf.Address{ipAddress}
+	}
+
+	// freedom protocol setting
+	var DomainStrategy string = "Asis"
+	if config.EnableDNS {
+		if config.DNSType != "" {
+			DomainStrategy = config.DNSType
+		} else {
+			DomainStrategy = "UseIP"
+	    }
 	}
 	proxySetting := &conf.FreedomConfig{
-		DomainStrategy: dnsSettings,
+		DomainStrategy: DomainStrategy,
 	}
 	var setting json.RawMessage
 	setting, err := json.Marshal(proxySetting)
